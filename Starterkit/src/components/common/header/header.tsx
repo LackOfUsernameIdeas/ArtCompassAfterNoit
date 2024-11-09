b13c4f6ffe76a0e96ef7162db1237258cb1edb57
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-11-09 23:05:38 +0200

    constant colors

--- Starterkit/src/components/common/header/header.tsx
+++ Starterkit/src/components/common/header/header.tsx
@@ -128,16 +128,6 @@ const Header: FC<HeaderProps> = ({ local_varaiable, ThemeChanger }: any) => {
                   Излизане
                 </button>
               </div>
-              <div className="header-element md:px-[0.48rem]">
-                <button
-                  aria-label="button"
-                  type="button"
-                  className="hs-dropdown-toggle switcher-icon inline-flex flex-shrink-0 justify-center items-center gap-2  rounded-full font-medium  align-middle transition-all text-xs dark:text-[#8c9097] dark:text-white/50 dark:hover:text-white dark:focus:ring-white/10 dark:focus:ring-offset-white/10"
-                  data-hs-overlay="#hs-overlay-switcher"
-                >
-                  <i className="bx bx-cog header-link-icon animate-spin-slow"></i>
-                </button>
-              </div>
             </div>
           </div>
         </nav>
