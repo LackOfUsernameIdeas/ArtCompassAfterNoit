a9a85bc1321470ca04486e37ac221a7fc0b17b4f
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-11-18 00:10:33 +0200

    We're getting somewhere

--- Starterkit/src/container/dashboards/home/Home.tsx
+++ Starterkit/src/container/dashboards/home/Home.tsx
@@ -19,6 +19,7 @@ import CountryBarChartComponent from "./Statistics/CountryBarChartComponent";
 import MovieProsperityBubbleChartComponent from "./Statistics/MovieProsperityBubbleChartComponent";
 import GenrePopularityOverTimeComponent from "./Statistics/GenrePopularityOverTimeComponent";
 import MovieBarChartComponent from "./Statistics/MovieBarChartComponent";
+import WidgetCards from "./Statistics/WidgetCards";
 
 interface CrmProps {}
 
@@ -46,19 +47,6 @@ const Home: FC<CrmProps> = () => {
     topCountries: []
   });
 
-  const [displayedNameAwards, setDisplayedNameAwards] = useState(
-    "Общ брой спечелени награди"
-  );
-  const [displayedValueAverages, setDisplayedValueAverages] =
-    useState<number>(0);
-  const [displayedNameAverages, setDisplayedNameAverages] =
-    useState("Среден Боксофис");
-
-  const [isAveragesMenuOpen, setIsAveragesMenuOpen] = useState(false);
-  const [isAwardsMenuOpen, setIsAwardsMenuOpen] = useState(false);
-
-  const [displayedValueAwards, setDisplayedValueAwards] = useState<number>(0);
-
   const pageSize = 5; // Number of entries per page for the chartaa
   const [currentChartPage, setCurrentChartPage] = useState(1); // Current page for the chart
   const [seriesDataForMovieBarChart, setSeriesDataForMovieBarChart] = useState<
@@ -85,22 +73,6 @@ const Home: FC<CrmProps> = () => {
       console.log("fetching");
     }
   }, []);
-
-  useEffect(() => {
-    if (
-      data.totalAwards.length > 0 &&
-      data.averageBoxOfficeAndScores.length > 0
-    ) {
-      setDisplayedValueAwards(data.totalAwards[0].total_awards_wins);
-      setDisplayedValueAverages(
-        data.averageBoxOfficeAndScores[0].average_box_office
-      );
-      console.log(
-        "BAR CHART RECOMMENDATIONS SHIT ---->",
-        data.topRecommendations
-      );
-    }
-  }, [data]);
 
   // Updated useEffect to sort data based on selected category
   useEffect(() => {
@@ -191,16 +163,6 @@ const Home: FC<CrmProps> = () => {
     );
   };
 
-  const toggleAwardsMenu = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    setIsAwardsMenuOpen((prev) => !prev);
-  };
-
-  const toggleAveragesMenu = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    setIsAveragesMenuOpen((prev) => !prev);
-  };
-
   const moviesAndSeriesCategoryDisplayNames: Record<
     "IMDb" | "Metascore" | "RottenTomatoes",
     string
@@ -227,278 +189,7 @@ const Home: FC<CrmProps> = () => {
         </div>
       </div>
       <div className="grid grid-cols-12 gap-x-6">
-        <div className="xxl:col-span-3 xl:col-span-3 col-span-12">
-          <div className="box custom-box">
-            <div className="box-body h-[5.5rem]">
-              <div className="flex items-center justify-between">
-                <div className="flex-grow">
-                  <p
-                    className={`mb-0 text-[#8c9097] dark:text-white/50 ${
-                      is1803 && "text-xs"
-                    }`}
-                  >
-                    Общ брой потребители
-                  </p>
-                  <div className="flex items-center">
-                    <span
-                      className={`text-[${
-                        is1803 ? "1.25rem" : "1.125rem"
-                      }] font-semibold`}
-                    >
-                      {data.usersCount?.[0]?.user_count || 0}
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="avatar avatar-md !rounded-full bg-primary/10 !text-secondary text-[1.125rem]">
-                    <i
-                      className={`bi bi-person text-primary text-[${
-                        is1803 ? "1rem" : "0.875rem"
-                      }]`}
-                    ></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="xxl:col-span-3 xl:col-span-3 col-span-12">
-          <div className="box custom-box">
-            <div className="box-body h-[5.5rem]">
-              <div className="flex items-center justify-between">
-                <div className="flex-grow">
-                  <p
-                    className={`mb-0 text-[#8c9097] dark:text-white/50 ${
-                      is1803 && "text-xs"
-                    }`}
-                  >
-                    Най-препоръчан жанр
-                  </p>
-                  <div className="flex items-center">
-                    <span
-                      className={`text-[${
-                        is1803 ? "1.25rem" : "1.125rem"
-                      }] font-semibold`}
-                    >
-                      {data.topGenres[0]?.genre_bg}
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="avatar avatar-md !rounded-full bg-primary/10 !text-secondary text-[1.125rem]">
-                    <i
-                      className={`bi bi-film text-primary text-[${
-                        is1803 ? "1rem" : "0.875rem"
-                      }]`}
-                    ></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="xxl:col-span-3 xl:col-span-3 col-span-12">
-          <div className="box custom-box">
-            <div className="box-body h-[5.5rem]">
-              <div className="flex items-center justify-between">
-                <div className="flex-grow">
-                  <div className="flex flex-wrap items-start">
-                    <div className="flex items-center space-x-2">
-                      <p
-                        className={`mb-0 text-[#8c9097] dark:text-white/50 ${
-                          is1803 &&
-                          "truncate overflow-hidden max-w-[130px] whitespace-nowrap text-xs"
-                        }`}
-                      >
-                        {displayedNameAverages}
-                      </p>
-                      <div className="hs-dropdown ti-dropdown">
-                        <Link
-                          to="#"
-                          className={`flex items-center ${
-                            is1803
-                              ? "px-1 py-0.5 text-xs"
-                              : "px-0.5 py-0.25 text-[0.70rem]"
-                          } font-medium text-primary border border-primary rounded-sm hover:bg-primary/10 transition-all`}
-                          onClick={toggleAveragesMenu}
-                          aria-expanded={isAveragesMenuOpen ? "true" : "false"}
-                        >
-                          {is1441 ? (
-                            <i
-                              className={`ri-arrow-${
-                                isAveragesMenuOpen ? "up" : "down"
-                              }-s-line text-sm`}
-                            ></i>
-                          ) : (
-                            <>
-                              <span>Сортирай по</span>
-                              <i
-                                className={`ri-arrow-${
-                                  isAveragesMenuOpen ? "up" : "down"
-                                }-s-line ml-0.5 text-sm`}
-                              ></i>
-                            </>
-                          )}
-                        </Link>
-                        <ul
-                          className={`hs-dropdown-menu ti-dropdown-menu ${
-                            isAveragesMenuOpen ? "block" : "hidden"
-                          }`}
-                          role="menu"
-                        >
-                          {averagesOptions.map(({ label, value }) => (
-                            <li key={label}>
-                              <Link
-                                onClick={() =>
-                                  handleDropdownClickAverages(
-                                    setDisplayedNameAverages,
-                                    setDisplayedValueAverages,
-                                    label,
-                                    value
-                                  )
-                                }
-                                className={`ti-dropdown-item ${
-                                  displayedNameAverages === label
-                                    ? "active"
-                                    : ""
-                                } ${
-                                  displayedNameAverages === label
-                                    ? "disabled"
-                                    : ""
-                                }`}
-                                to="#"
-                              >
-                                {label}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex items-center">
-                    <span
-                      className={`text-[${
-                        is1803 ? "1.25rem" : "1.125rem"
-                      }] font-semibold`}
-                    >
-                      {displayedValueAverages}
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="avatar avatar-md !rounded-full bg-primary/10 !text-secondary text-[1.125rem]">
-                    <i
-                      className={`bi bi-${
-                        displayedNameAverages == "Среден Боксофис"
-                          ? "ticket-perforated"
-                          : "bi bi-clipboard-data"
-                      } text-[${is1803 ? "1rem" : "0.875rem"}] text-primary`}
-                    ></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="xxl:col-span-3 xl:col-span-3 col-span-12">
-          <div className="box custom-box">
-            <div className="box-body h-[5.5rem]">
-              <div className="flex items-center justify-between">
-                <div className="flex-grow">
-                  <div
-                    className={`flex items-center space-x-${is1803 ? 2 : 1}`}
-                  >
-                    <p
-                      className={`mb-0 text-[#8c9097] dark:text-white/50 ${
-                        is1803 &&
-                        "truncate overflow-hidden max-w-[130px] whitespace-nowrap text-xs"
-                      }`}
-                    >
-                      {displayedNameAwards}
-                    </p>
-                    <div className="hs-dropdown ti-dropdown">
-                      <Link
-                        to="#"
-                        className={`flex items-center ${
-                          is1803
-                            ? "px-1 py-0.5 text-xs"
-                            : "px-0.5 py-0.25 text-[0.70rem]"
-                        } font-medium text-primary border border-primary rounded-sm hover:bg-primary/10 transition-all`}
-                        onClick={toggleAwardsMenu}
-                        aria-expanded={isAveragesMenuOpen ? "true" : "false"}
-                      >
-                        {is1441 ? (
-                          <i
-                            className={`ri-arrow-${
-                              isAwardsMenuOpen ? "up" : "down"
-                            }-s-line text-sm`}
-                          ></i>
-                        ) : (
-                          <>
-                            <span>Сортирай по</span>
-                            <i
-                              className={`ri-arrow-${
-                                isAwardsMenuOpen ? "up" : "down"
-                              }-s-line ml-0.5 text-sm`}
-                            ></i>
-                          </>
-                        )}
-                      </Link>
-                      <ul
-                        className={`hs-dropdown-menu ti-dropdown-menu ${
-                          isAwardsMenuOpen ? "block" : "hidden"
-                        }`}
-                        role="menu"
-                      >
-                        {awardOptions.map(({ label, value }) => (
-                          <li key={label}>
-                            <Link
-                              onClick={() =>
-                                handleDropdownClickAwards(
-                                  setDisplayedNameAwards,
-                                  setDisplayedValueAwards,
-                                  label,
-                                  value
-                                )
-                              }
-                              className={`ti-dropdown-item ${
-                                displayedNameAwards === label ? "active" : ""
-                              } ${
-                                displayedNameAwards === label ? "disabled" : ""
-                              }`}
-                              to="#"
-                            >
-                              {label}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="flex items-center">
-                    <span
-                      className={`text-[${
-                        is1803 ? "1.25rem" : "1.125rem"
-                      }] font-semibold`}
-                    >
-                      {displayedValueAwards}
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="avatar avatar-md !rounded-full bg-primary/10 !text-secondary text-[1.125rem]">
-                    <i
-                      className={`bi bi-trophy text-[${
-                        is1803 ? "1rem" : "0.875rem"
-                      }] text-primary`}
-                    ></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WidgetCards data={data} />
         <div className="xxl:col-span-6 col-span-12">
           <div className="xxl:col-span-6 col-span-12">
             <MovieProsperityBubbleChartComponent data={data} />
