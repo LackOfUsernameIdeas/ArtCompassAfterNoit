2e4502dcee9b1f023177cb58c0bc70817f3e3ad1
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-12-30 21:09:31 +0200

    separation

--- app/src/container/individualStats/IndividualStats.tsx
+++ app/src/container/individualStats/IndividualStats.tsx
@@ -9,6 +9,7 @@ import Notification from "../../components/common/notification/Notification";
 import { NotificationState } from "../recommendations/recommendations-types";
 import RecommendationsTable from "./Components/MoviesAndSeriesRecommendationsTable";
 import GenresBarChart from "./Components/GenresBarChart";
+import CountWidgets from "./Components/CountWidgets";
 
 interface IndividualStatsProps {}
 
@@ -158,71 +159,7 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
                         <GenresBarChart data={data} />
                       </div>
                       <div className="xxl:col-span-6 col-span-12">
-                        <div className="box custom-box">
-                          <div className="box-header justify-between">
-                            <div className="box-title">Брой Препоръчвани:</div>
-                          </div>
-                          <div className="box-body">
-                            <div className="grid grid-cols-1 xxl:grid-cols-3 gap-x-6">
-                              <div className="xxl:col-span-1 col-span-1">
-                                <div className="text-center">
-                                  <span className="avatar avatar-md bg-primary shadow-sm !rounded-full mb-2">
-                                    <i className="ri-movie-line text-[1rem]"></i>
-                                  </span>
-                                  <p className="text-[.875rem] font-semibold mb-2">
-                                    Филми
-                                  </p>
-                                  <div className="flex items-center justify-center flex-wrap">
-                                    <h5 className="mb-0 font-semibold">
-                                      {
-                                        data.topRecommendations
-                                          .recommendationsCount.movies
-                                      }
-                                    </h5>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="xxl:col-span-1 col-span-1">
-                                <div className="text-center">
-                                  <span className="avatar avatar-md bg-primary shadow-sm !rounded-full mb-2">
-                                    <i className="ri-movie-line text-[1rem]"></i>
-                                  </span>
-                                  <p className="text-[.875rem] font-semibold mb-2">
-                                    Сериали
-                                  </p>
-                                  <div className="flex items-center justify-center flex-wrap">
-                                    <h5 className="mb-0 font-semibold">
-                                      {
-                                        data.topRecommendations
-                                          .recommendationsCount.series
-                                      }
-                                    </h5>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="xxl:col-span-1 col-span-1">
-                                <div className="text-center">
-                                  <span className="avatar avatar-md bg-primary shadow-sm !rounded-full mb-2">
-                                    <i className="ri-movie-2-line text-[1rem]"></i>
-                                  </span>
-                                  <p className="text-[.875rem] font-semibold mb-2">
-                                    Общо
-                                  </p>
-                                  <div className="flex items-center justify-center flex-wrap">
-                                    <h5 className="mb-0 font-semibold">
-                                      {data.topRecommendations
-                                        .recommendationsCount.movies +
-                                        data.topRecommendations
-                                          .recommendationsCount.series}
-                                    </h5>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                        <CountWidgets data={data} />
                       </div>
                     </div>
                   </div>
