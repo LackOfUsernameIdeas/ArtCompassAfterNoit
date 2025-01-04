7bfe553e769d8492b7f31e381e4f17ded11cc201
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-01-04 18:26:08 +0200

    Update IndividualStats.tsx

--- app/src/container/individualStats/IndividualStats.tsx
+++ app/src/container/individualStats/IndividualStats.tsx
@@ -4,9 +4,7 @@ import {
   checkRecommendationExistsInWatchlist,
   checkTokenValidity,
   fetchData,
-  handleBookmarkClick,
-  removeFromWatchlist,
-  saveToWatchlist
+  handleBookmarkClick
 } from "./helper_functions";
 import { useNavigate } from "react-router-dom";
 import FadeInWrapper from "../../components/common/loader/fadeinwrapper";
@@ -19,109 +17,92 @@ import AccordionItem from "./Components/AccordionItem";
 interface IndividualStatsProps {}
 
 const IndividualStats: FC<IndividualStatsProps> = () => {
-  // Състояния за задържане на извлечени данни
   const [data, setData] = useState<DataType>({
     topRecommendations: {
-      recommendationsCount: {
-        movies: 0,
-        series: 0
-      },
+      recommendationsCount: { movies: 0, series: 0 },
       recommendations: []
-    }, // Топ препоръки
+    },
     topRecommendationsWatchlist: {
-      savedCount: {
-        movies: 0,
-        series: 0
-      },
+      savedCount: { movies: 0, series: 0 },
       watchlist: []
-    }, // Запазени филми/сериали в списък за гледане
-    topGenres: [], // Топ жанрове
-    topGenresWatchlist: [], // Топ запазени жанрове
-    sortedDirectorsByRecommendationCount: [], // Режисьори, сортирани по просперитет
-    sortedActorsByRecommendationCount: [], // Актьори, сортирани по просперитет
-    sortedWritersByRecommendationCount: [], // Сценаристи, сортирани по просперитет
-    sortedDirectorsBySavedCount: [], // Режисьори, сортирани по просперитет
-    sortedActorsBySavedCount: [], // Актьори, сортирани по просперитет
-    sortedWritersBySavedCount: [] // Сценаристи, сортирани по просперитет
+    },
+    topGenres: [],
+    topGenresWatchlist: [],
+    sortedDirectorsByRecommendationCount: [],
+    sortedActorsByRecommendationCount: [],
+    sortedWritersByRecommendationCount: [],
+    sortedDirectorsBySavedCount: [],
+    sortedActorsBySavedCount: [],
+    sortedWritersBySavedCount: []
   });
 
   const [notification, setNotification] = useState<NotificationState | null>(
     null
-  ); // Състояние за показване на известия (например съобщения за грешки, успехи или предупреждения)
+  );
   const [bookmarkedMovies, setBookmarkedMovies] = useState<{
     [key: string]: any;
   }>({});
-  const [alertVisible, setAlertVisible] = useState(false); // To control alert visibility
-  const [currentBookmarkStatus, setCurrentBookmarkStatus] = useState(false); // Track current bookmark status
-
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [currentBookmarkStatus, setCurrentBookmarkStatus] = useState(false);
   const [loading, setLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
+  // Utility function to get token
+  const getToken = () =>
+    localStorage.getItem("authToken") || sessionStorage.getItem("authToken");
+
+  // Handle notification close
   const handleNotificationClose = () => {
-    // Функция за затваряне на известията
-    if (notification?.type === "error") {
-      // Ако известието е от тип "грешка", пренасочване към страницата за вход
-      navigate("/signin");
-    }
-    setNotification(null); // Зануляване на известието
+    if (notification?.type === "error") navigate("/signin");
+    setNotification(null);
   };
 
+  // Validate token and show notification if expired
   useEffect(() => {
     const validateToken = async () => {
-      // Функция за проверка валидността на потребителския токен
-      const redirectUrl = await checkTokenValidity(); // Извикване на помощна функция за валидиране на токена
+      const redirectUrl = await checkTokenValidity();
       if (redirectUrl) {
-        // Ако токенът е невалиден, показване на известие
         showNotification(
-          setNotification, // Функция за задаване на известие
-          "Вашата сесия е изтекла. Моля, влезте в профила Ви отново.", // Съобщение за известието
-          "error" // Типът на известието (грешка)
+          setNotification,
+          "Вашата сесия е изтекла. Моля, влезте в профила Ви отново.",
+          "error"
         );
       }
     };
+    validateToken();
 
-    validateToken(); // Стартиране на проверката на токена при първоначално зареждане на компонента
-
-    const token =
-      localStorage.getItem("authToken") || sessionStorage.getItem("authToken"); // Вземане на токен от localStorage или sessionStorage
-
+    const token = getToken();
     if (token) {
       setLoading(true);
-      fetchData(token, setData, setLoading); // Извличане на данни с помощта на fetchData функцията
-      console.log("fetching"); // Лог за следене на извличането на данни
+      fetchData(token, setData, setLoading);
     }
   }, []);
 
+  // Handle loading bookmark status
   useEffect(() => {
     const loadBookmarkStatus = async () => {
-      const token =
-        localStorage.getItem("authToken") ||
-        sessionStorage.getItem("authToken");
+      const token = getToken();
+      if (!token) return;
 
-      if (token) {
-        const updatedBookmarks: { [key: string]: any } = {};
-        if (data.topRecommendationsWatchlist.watchlist) {
-          for (const movie of data.topRecommendationsWatchlist.watchlist) {
-            try {
-              const isBookmarked = await checkRecommendationExistsInWatchlist(
-                movie.imdbID,
-                token
-              );
-              if (isBookmarked) {
-                updatedBookmarks[movie.imdbID] = movie;
-              }
-            } catch (error) {
-              console.error("Error checking watchlist status:", error);
-            }
-          }
+      const updatedBookmarks: { [key: string]: any } = {};
+      for (const movie of data.topRecommendationsWatchlist.watchlist) {
+        try {
+          const isBookmarked = await checkRecommendationExistsInWatchlist(
+            movie.imdbID,
+            token
+          );
+          if (isBookmarked) updatedBookmarks[movie.imdbID] = movie;
+        } catch (error) {
+          console.error("Error checking watchlist status:", error);
         }
-        setBookmarkedMovies(updatedBookmarks);
       }
+      setBookmarkedMovies(updatedBookmarks);
     };
 
     loadBookmarkStatus();
   }, [data.topRecommendationsWatchlist.watchlist]);
 
+  // Loading state and empty data check
   if (loading) {
     return (
       <FadeInWrapper loadingTimeout={30000}>
@@ -131,12 +112,9 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
   }
 
   if (
-    !data.topRecommendations.recommendations ||
-    data.topRecommendations.recommendations.length === 0 ||
+    !data.topRecommendations.recommendations?.length ||
     !data.topGenres.length ||
-    !data.sortedDirectorsByRecommendationCount.length ||
-    !data.sortedActorsByRecommendationCount.length ||
-    !data.sortedWritersByRecommendationCount.length
+    !data.sortedDirectorsByRecommendationCount.length
   ) {
     return (
       <FadeInWrapper>
@@ -152,10 +130,6 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
     );
   }
 
-  const handleDismiss = () => {
-    setAlertVisible(false);
-  };
-
   return (
     <FadeInWrapper>
       {notification && (
@@ -168,15 +142,17 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
       {alertVisible && (
         <BookmarkAlert
           isBookmarked={currentBookmarkStatus}
-          onDismiss={handleDismiss}
+          onDismiss={() => setAlertVisible(false)}
         />
       )}
+
       <Fragment>
         <div className="md:flex block items-center justify-between my-[1.5rem] page-header-breadcrumb">
           <div>
             <p className="font-semibold text-[1.125rem] text-defaulttextcolor dark:text-defaulttextcolor/70 !mb-0 "></p>
           </div>
         </div>
+
         <div className="grid grid-cols-12 gap-6">
           <div className="xl:col-span-12 col-span-12">
             <div
@@ -199,11 +175,9 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
                     bookmarkedMovies={bookmarkedMovies}
                   />
                 </div>
-                {data.topRecommendationsWatchlist.watchlist ||
-                data.topGenresWatchlist.length ||
-                data.sortedDirectorsBySavedCount.length ||
-                data.sortedActorsBySavedCount.length ||
-                data.sortedWritersBySavedCount.length ? (
+                {(data.topRecommendationsWatchlist.watchlist ||
+                  data.topGenresWatchlist.length ||
+                  data.sortedDirectorsBySavedCount.length) && (
                   <AccordionItem
                     title="Моята Колекция за Гледане - Статистики"
                     type="watchlist"
@@ -211,7 +185,7 @@ const IndividualStats: FC<IndividualStatsProps> = () => {
                     handleBookmarkClick={handleBookmarkClick}
                     bookmarkedMovies={bookmarkedMovies}
                   />
-                ) : null}
+                )}
               </div>
             </div>
           </div>
