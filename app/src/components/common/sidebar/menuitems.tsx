dcbeadd5dc5fc48178e9ffd1336bdbb2b27a9cf7
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-01-11 21:32:36 +0200

    app/test

--- app/src/components/common/sidebar/menuitems.tsx
+++ app/src/components/common/sidebar/menuitems.tsx
@@ -66,17 +66,6 @@ export const MENUITEMS = [
     ]
   },
   {
-    path: `${import.meta.env.BASE_URL}app/test`,
-    icon: <i className="side-menu__icon bx bx-movie-play"></i>,
-    type: "link",
-    Name: "",
-    active: false,
-    selected: false,
-    title: "Тест",
-    class:
-      "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2"
-  },
-  {
     menutitle: "КОНТАКТ"
   },
   {
