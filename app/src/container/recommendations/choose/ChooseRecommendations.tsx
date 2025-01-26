619c76a2c0975f7f62cafe0eec057550bbde5ad9
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-01-26 11:24:31 +0200

    Update ChooseRecommendations.tsx

--- app/src/container/recommendations/choose/ChooseRecommendations.tsx
+++ app/src/container/recommendations/choose/ChooseRecommendations.tsx
@@ -1,11 +1,10 @@
 import { FC, useState, useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
 import { useNavigate } from "react-router-dom";
 import FadeInWrapper from "../../../components/common/loader/fadeinwrapper";
 
 const ChooseRecommendations: FC = () => {
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true); // Initially set loading to true
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   const question = {
@@ -13,12 +12,12 @@ const ChooseRecommendations: FC = () => {
     options: ["Филми и сериали", "Книги"]
   };
 
-  // Set loading to false after the component mounts (simulating a page load delay)
+  // Simulating a page load delay
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 100); // Delay for 1 second before hiding the loader
-    return () => clearTimeout(timer); // Cleanup timeout on unmount
+    }, 100);
+    return () => clearTimeout(timer);
   }, []);
 
   const handleAnswerClick = (answer: string) => {
@@ -28,7 +27,6 @@ const ChooseRecommendations: FC = () => {
   const handleClick = () => {
     if (!selectedAnswer) return;
 
-    // Forward to the respective route based on the selected answer
     if (selectedAnswer === "Филми и сериали") {
       navigate("/app/recommendations/movies_series");
     } else if (selectedAnswer === "Книги") {
@@ -36,54 +34,56 @@ const ChooseRecommendations: FC = () => {
     }
   };
 
+  console.log("selectedAnswer: ", selectedAnswer);
   return (
     <FadeInWrapper>
       <div className="flex items-center justify-center px-4">
-        <CSSTransition
-          in={!loading}
-          timeout={500}
-          classNames="fade"
-          unmountOnExit
-        >
-          <div className="w-full max-w-4xl py-8 px-4">
-            <div className="question bg-opacity-70 border-2 text-white rounded-lg p-4 glow-effect transition-all duration-300">
-              <h2 className="text-xl font-semibold break-words">
-                {question.question}
-              </h2>
-            </div>
-
-            <div className="mt-8">
-              {question.options.map((option) => (
-                <div key={option} className="my-4">
-                  <button
-                    onClick={() => handleAnswerClick(option)}
-                    className={`w-full py-2 px-4 bg-secondary text-white rounded-lg ${
-                      selectedAnswer === option
-                        ? "bg-opacity-80"
-                        : "bg-opacity-60"
-                    } transition-all duration-300`}
-                  >
-                    {option}
-                  </button>
-                </div>
-              ))}
-            </div>
-
-            <div className="mt-8">
-              <button
-                onClick={handleClick}
-                className={`next glow-next bg-opacity-70 text-white font-bold rounded-lg p-6 mt-4 flex justify-center items-center transition-all duration-200 ${
-                  selectedAnswer && selectedAnswer.length > 0
-                    ? "opacity-100 pointer-events-auto cursor-pointer"
-                    : "opacity-0 pointer-events-none"
-                }`}
-                disabled={!selectedAnswer}
-              >
-                Следващ въпрос
-              </button>
-            </div>
+        <div className="w-full max-w-4xl py-8 px-4">
+          {/* Question Header */}
+          <div className="question bg-opacity-70 border-2 text-white rounded-lg p-4 glow-effect transition-all duration-300">
+            <h2 className="text-xl font-semibold break-words">
+              {question.question}
+            </h2>
           </div>
-        </CSSTransition>
+
+          {/* Grid Layout for Options */}
+          <div
+            className={`grid gap-4 mt-8 ${
+              (question.options.length ?? 0) > 6
+                ? "grid-cols-2 md:grid-cols-5"
+                : "grid-cols-1"
+            }`}
+          >
+            {question.options.map((option) => (
+              <div
+                key={option}
+                onClick={() => handleAnswerClick(option)}
+                className={`${
+                  selectedAnswer === option
+                    ? "selected-answer transform scale-105"
+                    : "question hover:bg-secondary hover:text-white"
+                } bg-opacity-70 p-6 text-white rounded-lg glow-effect transition-all duration-300 cursor-pointer flex justify-center items-center text-center`}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+
+          {/* Next Button */}
+          <div className="mt-8">
+            <button
+              onClick={handleClick}
+              className={`next glow-next bg-opacity-70 text-white font-bold rounded-lg p-6 mt-4 flex justify-center items-center transition-all duration-200 ${
+                selectedAnswer
+                  ? "opacity-100 pointer-events-auto cursor-pointer"
+                  : "opacity-0 pointer-events-none"
+              }`}
+              disabled={!selectedAnswer}
+            >
+              Следващ въпрос
+            </button>
+          </div>
+        </div>
       </div>
     </FadeInWrapper>
   );
