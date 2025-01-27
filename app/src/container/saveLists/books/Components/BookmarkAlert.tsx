dbd3b470d03a1efbe05640528b9b505814cf3861
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-01-27 20:22:15 +0200

    Update BookmarkAlert.tsx

--- app/src/container/saveLists/books/Components/BookmarkAlert.tsx
+++ app/src/container/saveLists/books/Components/BookmarkAlert.tsx
@@ -47,8 +47,8 @@ const BookmarkAlert: React.FC<BookmarkAlertProps> = ({
             </svg>
             <span>
               {isBookmarked
-                ? "Добавено в списък за гледане"
-                : "Премахнато от списък за гледане"}
+                ? "Добавено в списък за четене"
+                : "Премахнато от списък за четене"}
             </span>
           </div>
           <button
@@ -77,8 +77,8 @@ const BookmarkAlert: React.FC<BookmarkAlertProps> = ({
         </div>
         <div className="pl-8 text-[0.75rem] opacity-[0.8] mb-1">
           {isBookmarked
-            ? "Вашият филм/сериал е запазен в списъка ви за гледане!"
-            : "Този филм/сериал е премахнат от списъка ви за гледане!"}
+            ? "Вашата книга е запазена в списъка ви за четене!"
+            : "Тази книга е премахната от списъка ви за четене!"}
         </div>
       </div>
     </div>
