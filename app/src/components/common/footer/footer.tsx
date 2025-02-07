ff37117c6d596ceb74bdec5f1543a25c307b4e04
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-02-07 17:15:57 +0200

    colors

--- app/src/components/common/footer/footer.tsx
+++ app/src/components/common/footer/footer.tsx
@@ -24,7 +24,7 @@ const Footer: FC = () => (
           <div className="footer-content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             <div className="footer-column">
               <div className="brand-name">
-                <span className="footer-brand-name-art dark:text-[#9A110A] text-[#610000]">
+                <span className="footer-brand-name-art dark:text-[#AD0C48] text-[#9A110A]">
                   АРТ
                 </span>
                 <span className="footer-brand-name-compass">КОМПАС</span>
