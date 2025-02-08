5deb784528b9b148ad47f17a9f045952dd936d5a
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-02-08 20:48:35 +0200

    translated stuff

--- app/src/components/common/widget/widget.tsx
+++ app/src/components/common/widget/widget.tsx
@@ -5,12 +5,13 @@ const Widget: FC<{
   title: string;
   value: string | number;
 }> = ({ icon, title, value }) => (
-  <div className="bg-white dark:bg-bodybg2 dark:text-defaulttextcolor/70 p-4 rounded-lg transition-all duration-300 hover:shadow-md dark:hover:shadow-primary">
+  <div className="bg-white dark:bg-bodybg2 dark:text-defaulttextcolor/70 p-4 rounded-lg transition-all duration-300 hover:shadow-md dark:hover:shadow-primary flex flex-col justify-between">
     <div className="flex items-center mb-2">
       {icon}
       <h3 className="ml-2 text-sm font-semibold">{title}</h3>
     </div>
-    <p className="text-2xl font-bold">{value}</p>
+    <div className="flex-grow" />
+    <p className="text-2xl font-bold text-left">{value}</p>
   </div>
 );
 
--- app/src/container/aiAnalysator/Components/AIAnalysisDashboard.tsx
+++ app/src/container/aiAnalysator/Components/AIAnalysisDashboard.tsx
@@ -10,49 +10,48 @@ const AIAnalysisDashboard: FC<AIAnalysisDashboardProps> = ({
 }) => {
   return (
     <div className="bg-bodybg mt-4 p-6 rounded-xl shadow-lg space-y-6">
-      <h2 className="text-2xl font-bold mb-4">AI Analysis Dashboard</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
         <MainMetricsWidget
-          icon={<i className="ti ti-bullseye text-3xl"></i>}
+          icon={<i className="ti ti-percentage-60 text-2xl"></i>}
           title="Precision"
           value={`${precisionData.precision_percentage.toFixed(2)}%`}
-          description={`${precisionData.relevant_recommendations_count} out of ${precisionData.total_recommendations_count} recommendations were relevant`}
+          description={`${precisionData.relevant_recommendations_count} от общо ${precisionData.total_recommendations_count} препоръки, които сте направили, са релевантни`}
           progress={precisionData.precision_percentage}
         />
         <MainMetricsWidget
-          icon={<i className="ti ti-zoom-scan text-3xl"></i>}
+          icon={<i className="ti ti-percentage-40 text-2xl"></i>}
           title="Recall"
           value={`${recallData.recall_percentage.toFixed(2)}%`}
-          description={`${recallData.relevant_user_recommendations_count} out of ${recallData.relevant_platform_recommendations_count} relevant recommendations were made`}
+          description={`${recallData.relevant_user_recommendations_count} от общо ${recallData.relevant_platform_recommendations_count} релевантни препоръки в системата са отправени към вас`}
           progress={recallData.recall_percentage}
         />
         <MainMetricsWidget
-          icon={<i className="ti ti-chart-bar text-3xl"></i>}
+          icon={<i className="ti ti-percentage-70 text-2xl"></i>}
           title="F1 Score"
           value={`${f1ScoreData.f1_score_percentage.toFixed(2)}%`}
-          description="Balance between Precision and Recall"
+          description="Баланс между Precision и Recall"
           progress={f1ScoreData.f1_score_percentage}
         />
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
         <Widget
-          icon={<i className="ti ti-list-numbers text-3xl"></i>}
-          title="Total Recommendations"
+          icon={<i className="ti ti-list-numbers text-3xl" />}
+          title="Общ брой препоръки"
           value={precisionData.total_recommendations_count}
         />
         <Widget
-          icon={<i className="ti ti-check-list text-3xl"></i>}
-          title="Relevant Recommendations"
+          icon={<i className="ti ti-copy-check text-3xl" />}
+          title="Релевантни препоръки"
           value={precisionData.relevant_recommendations_count}
         />
         <Widget
-          icon={<i className="ti ti-server text-3xl"></i>}
-          title="Platform Recommendations"
+          icon={<i className="ti ti-database text-3xl" />}
+          title="Общ брой препоръки в платформата"
           value={recallData.total_platform_recommendations_count}
         />
         <Widget
-          icon={<i className="ti ti-check-circle text-3xl"></i>}
-          title="Relevant Platform Recommendations"
+          icon={<i className="ti ti-checkbox text-3xl" />}
+          title="Релевантни препоръки в платформата"
           value={recallData.relevant_platform_recommendations_count}
         />
       </div>
