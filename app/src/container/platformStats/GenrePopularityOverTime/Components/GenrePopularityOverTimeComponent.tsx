ef4e6241d2fffd723c46e8e581b110717f817626
Maria Malcheva <mimimalceva@gmail.com>
2025-02-10 13:09:22 +0200

    genrepopularity explanations v1

--- app/src/container/platformStats/GenrePopularityOverTime/Components/GenrePopularityOverTimeComponent.tsx
+++ app/src/container/platformStats/GenrePopularityOverTime/Components/GenrePopularityOverTimeComponent.tsx
@@ -2,6 +2,12 @@ import { FC, Fragment } from "react";
 import { GenrePopularityOverTimeDataType } from "../../platformStats-types";
 import { generateHeatmapSeriesData } from "../../helper_functions";
 import { GenrePopularityOverTime } from "../../charts";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+} from "@/components/ui/accordion";
 
 interface GenrePopularityOverTimeComponentProps {
   data: GenrePopularityOverTimeDataType;
@@ -17,6 +23,13 @@ const GenrePopularityOverTimeComponent: FC<
   return (
     <Fragment>
       <div className="xxl:col-span-12 xl:col-span-12 col-span-12">
+          {/* Лява част */}
+          <div className="!text-lg box p-6 flex flex-col ">
+            <p className="text-center">
+              Тук може да видите колко пъти са препоръчвани различните жанровете филми излезнали през съответната годината.<br/>
+              По-тъмните квадрати означават по-голяма популярност, докато по-светлите – по-малка. 
+            </p>
+          </div>
         <div className="box">
           <div className="box-header !gap-0 !m-0 justify-between">
             <div className="box-title opsilion">
