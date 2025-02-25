979af7bf3a255baea44fbc124697d4dd81042d0d
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-02-25 21:30:29 +0200

    landing bug

--- app/src/components/common/infobox/infobox.tsx
+++ app/src/components/common/infobox/infobox.tsx
@@ -10,7 +10,7 @@ const Infobox: FC<InfoboxProps> = ({ onClick, width = 22, height = 22 }) => {
   return (
     <div
       onClick={onClick}
-      className={`dark:text-defaulttextcolor/70 cursor-pointer transition-transform duration-200 hover:scale-110 rounded-full`}
+      className={`dark:text-defaulttextcolor/70 cursor-pointer transition-transform duration-200 hover:scale-110 rounded-full z-10`}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
