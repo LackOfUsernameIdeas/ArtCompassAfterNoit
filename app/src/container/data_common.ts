6928975d03fd2cf5ad82ca575f29806733c63b0f
Ivaylo Zdravkov <133565939+ivajlozdr@users.noreply.github.com>
2025-03-23 18:26:57 +0200

    some clean up

--- app/src/container/data_common.ts
+++ app/src/container/data_common.ts
@@ -1,3 +1,20 @@
+import img0 from "@/assets/images/steps/0.jpg";
+import img1_1 from "@/assets/images/steps/1.1.png";
+import img1_2 from "@/assets/images/steps/1.2.png";
+import img1_3 from "@/assets/images/steps/1.3.png";
+import img1_4 from "@/assets/images/steps/1.4.png";
+import img2 from "@/assets/images/steps/2.jpg";
+import img3_1 from "@/assets/images/steps/3.1.png";
+import img3_2 from "@/assets/images/steps/3.2.png";
+import img3_3 from "@/assets/images/steps/3.3.png";
+import img3_4 from "@/assets/images/steps/3.4.png";
+import img4_1 from "@/assets/images/steps/4.1.png";
+import img4_2 from "@/assets/images/steps/4.2.png";
+import img5 from "@/assets/images/steps/5.png";
+import img6_1 from "@/assets/images/steps/6.1.png";
+import img6_2 from "@/assets/images/steps/6.2.png";
+import img6_3 from "@/assets/images/steps/6.3.png";
+
 /**
  * Мапинг на възможните опции за предпочитания.
  *
@@ -122,4 +139,50 @@ export const goodreadsGenreOptions: Array<{ en: string; bg: string }> = [
   { en: "Thriller", bg: "Трилър" },
   { en: "Travel", bg: "Пътешествия" },
   { en: "Young Adult", bg: "Младежка литература" }
+];
+
+// Стъпки за успешно съставяне на мозъчен анализ
+export const steps = [
+  {
+    step: "0. Въведение",
+    description:
+      "За да започнете мозъчния анализ, трябва да се сдобиете с устройството NeuroSky MindWave Mobile 2. Следвайте следващите стъпки, за да го използвате успешно.",
+    images: [img0]
+  },
+  {
+    step: "1. Сваляне на ThinkGear програмата - mwm2.neurosky.com",
+    description:
+      "Оттам избирате от опциите за изтегляне, спрямо вашата операционната система. След това разархивирате сваления файл и стартирате ThinkGear Connector - зелената иконка. Трябва да се появи долу вдясно иконка, наподобяваща мозък.",
+    images: [img1_1, img1_2, img1_3, img1_4]
+  },
+  {
+    step: "2. Пускане на устройството.",
+    description: "Излиза синя светлина, когато е пуснато.",
+    images: [img2]
+  },
+  {
+    step: "3. Свързване на устройството.",
+    description:
+      "Трябва вашият компютър да поддържа Bluetooth (или да имате Bluetooth Adapter). Пускате го от настройките и натискате на опцията за добавяне на ново устройство. Свързвате се към MindWave Mobile (изчаквате, докато не видите иконката със СЛУШАЛКИ).",
+    images: [img3_1, img3_2, img3_3, img3_4]
+  },
+  {
+    step: "4. Конфигуриране на COM порт.",
+    description:
+      "След успешно свързване, отивате на More Bluetooth options и оттам в раздела COM ports. Трябва да видите на кой от тях е свързано устройството и ако не е, да добавите порт към него. Интересува ни OUTGOING порта. Той се обозначава с COM и съответната цифра (в примера от снимките, това е COM4). След като идентифицирате правилния порт, отивате и го пишете (пример: „COM4“) в ThinkGear Connector приложението на показаното поле.",
+    images: [img4_1, img4_2]
+  },
+  {
+    step: "5. Сваляне на програмата за свързване с АртКомпас",
+    description:
+      "След сваляне на архива го разархивирате и отваряте. Трябва да виждате .exe файла за свързване.",
+    images: [img5],
+    fileName: "artcompass_brain_analysis.zip"
+  },
+  {
+    step: "6. Същинско свързване",
+    description:
+      "Ако първо сте пуснали ThinkGear Connector, след което сте включили устройството, след което Bluetooth на вашия компютър и най-накрая нашата програмата и видите, че иконката на ThinkGear долу вдясно е в синьо и пише, че има връзка с устройството и също така ви излезе прозореца за започване на сесията, значи успешно сте свързали устройството. **Ако все пак не излиза в command prompt-а съобщението за успешно свързване и не се появява прозореца на програмата, но въпреки това ThinkGear посочва, че има връзка, опитайте да рестартирате.**",
+    images: [img6_1, img6_2, img6_3]
+  }
 ];
