40ecd88b0bf1b31e790d949a5b19568d42f31312
LackOfUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-07-10 22:54:48 +0300

    na ti abstrakciq, Ivailo

--- app/src/redux/reducer.tsx
+++ app/src/redux/reducer.tsx
@@ -74,3 +74,46 @@ export default function reducer(state = initialState, action: any) {
       return state;
   }
 }
+
+export function reducerLanding(state = initialState, action: any) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "ThemeChanger":
+      state = {
+        ...payload,
+        colorPrimaryRgb: payload.class === "dark" ? "175 11 72" : "154 17 10",
+        colorPrimary: payload.class === "dark" ? "175 11 72" : "154 17 10",
+        colorSecondaryRgb: payload.class === "dark" ? "256 52 12" : "193 46 23",
+        colorSecondary: payload.class === "dark" ? "256 52 12" : "193 46 23",
+        colorTertiaryRgb: payload.class === "dark" ? "210, 102, 0" : "107 21 2",
+        colorTertiary: payload.class === "dark" ? "210, 102, 0" : "107 21 2",
+        lang: "bg",
+        dir: "ltr",
+        dataNavLayout: "horizontal",
+        dataVerticalStyle: "overlay",
+        toggled: "close",
+        dataNavStyle: "menu-click",
+        horStyle: "",
+        dataPageStyle: "modern",
+        dataWidth: "fullwidth",
+        dataMenuPosition: "fixed",
+        dataHeaderPosition: "fixed",
+        loader: "disable",
+        iconOverlay: "",
+        bodyBg: "",
+        Light: "",
+        darkBg: "",
+        inputBorder: "",
+        bgImg: "",
+        iconText: "",
+        body: {
+          class: ""
+        }
+      };
+      return state;
+
+    default:
+      return state;
+  }
+}
